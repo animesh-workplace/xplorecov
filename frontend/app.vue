b2fa5ca40d5927802938c827030de7a2eb95b092
Animesh Kumar Singh <animesh.workplace@gmail.com>
2024-12-19 16:11:03 +0530

    updated file - frontend/app.vue

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,12 @@
 <template>
 	<div>
+		<Notivue v-slot="item">
+			<Notification :item="item" :theme="materialTheme" />
+		</Notivue>
 		<NuxtPage />
 	</div>
 </template>
+
+<script setup>
+import { materialTheme } from 'notivue'
+</script>
