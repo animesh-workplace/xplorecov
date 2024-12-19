fbd80a4f0da8916d04d9a42ea1d7c62befe2ae0e
Animesh Kumar Singh <animesh.workplace@gmail.com>
2024-12-19 11:40:19 +0530

    updated file - frontend/nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,34 +1,33 @@
-import Aura from "@primevue/themes/aura";
+import Aura from '@primevue/themes/aura'
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  modules: [
-    "@primevue/nuxt-module",
-    "nuxt-echarts",
-    "@pinia/nuxt",
-    "@nuxtjs/tailwindcss",
-    "@nuxt/image",
-    "@nuxt/icon",
-    "@vueuse/nuxt",
-    "nuxt-umami",
-    "dayjs-nuxt",
-  ],
-  primevue: {
-    options: {
-      ripple: true,
-      theme: { preset: Aura },
-    },
-  },
-  umami: { enabled: false },
-  echarts: {
-    ssr: true,
-    charts: ["BarChart"],
-    components: [
-      "GridComponent",
-      "DatasetComponent",
-      "TooltipComponent",
-      "ToolboxComponent",
-    ],
-  },
-});
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	modules: [
+		'dayjs-nuxt',
+		'nuxt-umami',
+		'@nuxt/icon',
+		'@pinia/nuxt',
+		'@nuxt/fonts',
+		'@nuxt/image',
+		'nuxt-lodash',
+		'nuxt-echarts',
+		'@nuxt/eslint',
+		'@vueuse/nuxt',
+		'@nuxtjs/device',
+		'@nuxtjs/tailwindcss',
+		'@primevue/nuxt-module',
+	],
+	primevue: {
+		options: {
+			ripple: true,
+			theme: { preset: Aura },
+		},
+	},
+	umami: { enabled: false, id: '12d666c0-d0bf-4271-ae3b-2ff52f81be58', host: 'https://research.nibmg.ac.in' },
+	echarts: {
+		ssr: true,
+		charts: ['BarChart'],
+		components: ['GridComponent', 'DatasetComponent', 'TooltipComponent', 'ToolboxComponent'],
+	},
+})
