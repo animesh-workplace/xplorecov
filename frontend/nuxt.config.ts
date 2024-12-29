2b7c470e6b5984ec23e49913163735802643a4df
Animesh Kumar Singh <animesh.workplace@gmail.com>
2024-12-29 16:57:36 +0530

    updated file - frontend/nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,37 @@
 import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primevue/themes'
+
+const Xaura = definePreset(Aura, {
+	semantic: {
+		colorScheme: {
+			dark: {
+				surface: {
+					0: '#ffffff',
+					50: '{neutral.50}',
+					100: '{neutral.100}',
+					200: '{neutral.200}',
+					300: '{neutral.300}',
+					400: '{neutral.400}',
+					500: '{neutral.500}',
+					600: '{neutral.600}',
+					700: '{neutral.700}',
+					800: '{neutral.800}',
+					900: '{neutral.700}',
+					950: '{neutral.950}',
+				},
+			},
+		},
+	},
+})
 
 export default defineNuxtConfig({
-	app: { head: { title: 'XPLORECoV | National Institute of Biomedical Genomics', meta: [], link: [] } },
+	app: {
+		head: {
+			title: 'XPLORECoV | National Institute of Biomedical Genomics',
+			meta: [],
+			link: [],
+		},
+	},
 	modules: [
 		'dayjs-nuxt',
 		'nuxt-umami',
@@ -27,13 +57,14 @@ export default defineNuxtConfig({
 	notivue: { position: 'bottom-right' },
 	dayjs: { plugins: ['customParseFormat'] },
 	umami: { enabled: false, id: '12d666c0-d0bf-4271-ae3b-2ff52f81be58', host: 'https://research.nibmg.ac.in' },
-	primevue: { options: { ripple: true, theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } } },
+	primevue: { options: { ripple: true, theme: { preset: Xaura, options: { darkModeSelector: '.dark' } } } },
 	css: [
 		'primeicons/primeicons.css',
 		'notivue/notification.css',
 		'notivue/animations.css',
 		'@/assets/css/main.css',
 		'@/assets/css/fonts.css',
+		'@/assets/css/jquery.mCustomScrollbar.css',
 	],
 	// build: { transpile: ['lodash'] },
 })
