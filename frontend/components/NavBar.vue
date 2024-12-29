f7ce603812bda95c0f32745dbb6eeab742ff24de
Animesh Kumar Singh <animesh.workplace@gmail.com>
2024-12-29 16:57:36 +0530

    updated file - frontend/components/NavBar.vue

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
 	<nav
-		class="bg-white dark:bg-gray-900 fixed w-full top-0 start-0 border-b border-gray-200 dark:border-gray-600 z-50"
+		class="bg-white dark:bg-neutral-800 fixed w-full top-0 start-0 border-b border-gray-200 dark:border-neutral-600 z-50"
 	>
 		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 			<NuxtLink href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -44,7 +44,7 @@
 				id="navbar-sticky"
 			>
 				<ul
-					class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+					class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-neutral-800 dark:border-gray-700"
 				>
 					<li>
 						<NuxtLink
