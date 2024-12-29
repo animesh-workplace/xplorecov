3dbe0aebe86d2449bf4935b0ba64537a17d1fb96
Animesh Kumar Singh <animesh.workplace@gmail.com>
2024-12-29 11:12:42 +0530

    updated file - frontend/components/Logo.vue

---
+++ frontend/components/Logo.vue
@@ -0,0 +1,357 @@
+<template>
+	<svg
+		width="640"
+		height="640"
+		version="1.1"
+		viewBox="0 0 640 640"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		class="Spinner-sc-10nq2ph-0 faqfqd shift-circle-mobile"
+		data-v-0c35f2a4=""
+		data-v-3fbd1312=""
+	>
+		<defs data-v-0c35f2a4="">
+			<rect id="path-1" x="0" y="0" width="640" height="640" data-v-0c35f2a4=""></rect>
+			<rect id="path-3" x="0" y="0" width="376" height="376" data-v-0c35f2a4=""></rect>
+		</defs>
+		<g id="Designs" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" data-v-0c35f2a4="">
+			<g id="Group-67" data-v-0c35f2a4="">
+				<g id="outer" data-v-0c35f2a4="">
+					<mask id="mask-2" fill="white" data-v-0c35f2a4="">
+						<use xlink:href="#path-1" data-v-0c35f2a4=""></use>
+					</mask>
+					<g id="Rectangle-23" data-v-0c35f2a4=""></g>
+					<g id="green" mask="url(#mask-2)" data-v-0c35f2a4="">
+						<g
+							transform="translate(90.500000, 254.000000) scale(-1, 1) rotate(38.000000) translate(-90.500000, -254.000000) translate(-66.000000, 126.000000)"
+							data-v-0c35f2a4=""
+						>
+							<path
+								fill="none"
+								id="Path-16"
+								stroke="#087FD2"
+								stroke-width="30"
+								stroke-linecap="round"
+								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
+								data-v-0c35f2a4=""
+							></path>
+							<circle
+								r="15"
+								cx="15.31"
+								cy="15.86"
+								id="Oval-7"
+								stroke="none"
+								fill="#000000"
+								fill-opacity="0.16"
+								fill-rule="evenodd"
+								data-v-0c35f2a4=""
+							></circle>
+						</g>
+					</g>
+					<g id="green" mask="url(#mask-2)" data-v-0c35f2a4="">
+						<g
+							transform="translate(388.171879, 91.171879) scale(-1, 1) rotate(-53.000000) translate(-388.171879, -91.171879) translate(231.671879, -36.828121)"
+							data-v-0c35f2a4=""
+						>
+							<path
+								fill="none"
+								id="Path-16"
+								stroke="#52A5E0"
+								stroke-width="30"
+								stroke-linecap="round"
+								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
+								data-v-0c35f2a4=""
+							></path>
+							<circle
+								r="15"
+								cx="15.31"
+								cy="15.86"
+								id="Oval-7"
+								stroke="none"
+								fill="#000000"
+								fill-opacity="0.16"
+								fill-rule="evenodd"
+								data-v-0c35f2a4=""
+							></circle>
+						</g>
+					</g>
+					<g id="green" mask="url(#mask-2)" data-v-0c35f2a4="">
+						<g
+							transform="translate(547.532976, 388.101252) scale(-1, 1) rotate(-143.000000) translate(-547.532976, -388.101252) translate(391.032976, 260.101252)"
+							data-v-0c35f2a4=""
+						>
+							<path
+								fill="none"
+								id="Path-16"
+								stroke="#CEE5F6"
+								stroke-width="30"
+								stroke-linecap="round"
+								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
+								data-v-0c35f2a4=""
+							></path>
+							<circle
+								r="15"
+								cx="15.31"
+								cy="15.86"
+								id="Oval-7"
+								stroke="none"
+								fill="#000000"
+								fill-opacity="0.16"
+								fill-rule="evenodd"
+								data-v-0c35f2a4=""
+							></circle>
+						</g>
+					</g>
+					<g id="green" mask="url(#mask-2)" data-v-0c35f2a4="">
+						<g
+							transform="translate(251.171879, 547.171879) scale(-1, 1) rotate(-233.000000) translate(-251.171879, -547.171879) translate(94.671879, 419.171879)"
+							data-v-0c35f2a4=""
+						>
+							<path
+								fill="none"
+								id="Path-16"
+								stroke="#065F9E"
+								stroke-width="30"
+								stroke-linecap="round"
+								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
+								data-v-0c35f2a4=""
+							></path>
+							<circle
+								r="15"
+								cx="15.31"
+								cy="15.86"
+								id="Oval-7"
+								stroke="none"
+								fill="#000000"
+								fill-opacity="0.16"
+								fill-rule="evenodd"
+								data-v-0c35f2a4=""
+							></circle>
+						</g>
+					</g>
+				</g>
+				<g id="mid" data-v-0c35f2a4="">
+					<g transform="translate(76.000000, 76.000000)" data-v-0c35f2a4="">
+						<g id="Group-68" transform="translate(242.000000, 6.000000)" data-v-0c35f2a4="">
+							<path
+								id="Path-13"
+								stroke="#CEE5F6"
+								stroke-width="30"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
+								data-v-0c35f2a4=""
+							></path>
+							<circle
+								r="15"
+								cx="228"
+								cy="172"
+								id="Oval-6"
+								fill="#000000"
+								fill-opacity="0.16"
+								data-v-0c35f2a4=""
+							></circle>
+						</g>
+						<g
+							id="Group-68"
+							transform="translate(386.500000, 364.500000) rotate(-270.000000) translate(-386.500000, -364.500000) translate(265.000000, 271.000000)"
+							data-v-0c35f2a4=""
+						>
+							<path
+								id="Path-13"
+								stroke="#52A5E0"
+								stroke-width="30"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
+								data-v-0c35f2a4=""
+							></path>
+							<circle
+								r="15"
+								cx="228"
+								cy="172"
+								id="Oval-6"
+								fill="#000000"
+								fill-opacity="0.16"
+								data-v-0c35f2a4=""
+							></circle>
+						</g>
+						<g
+							id="Group-68"
+							transform="translate(121.500000, 386.500000) rotate(-180.000000) translate(-121.500000, -386.500000) translate(0.000000, 293.000000)"
+							data-v-0c35f2a4=""
+						>
+							<path
+								id="Path-13"
+								stroke="#087FD2"
+								stroke-width="30"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
+								data-v-0c35f2a4=""
+							></path>
+							<circle
+								r="15"
+								cx="228"
+								cy="172"
+								id="Oval-6"
+								fill="#000000"
+								fill-opacity="0.16"
+								data-v-0c35f2a4=""
+							></circle>
+						</g>
+						<g
+							id="Group-68"
+							transform="translate(99.500000, 121.500000) rotate(-90.000000) translate(-99.500000, -121.500000) translate(-22.000000, 28.000000)"
+							data-v-0c35f2a4=""
+						>
+							<path
+								id="Path-13"
+								stroke="#065F9E"
+								stroke-width="30"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
+								data-v-0c35f2a4=""
+							></path>
+							<circle
+								r="15"
+								cx="228"
+								cy="172"
+								id="Oval-6"
+								fill="#000000"
+								fill-opacity="0.16"
+								data-v-0c35f2a4=""
+							></circle>
+						</g>
+					</g>
+				</g>
+				<g id="inner" data-v-0c35f2a4="">
+					<g transform="translate(131.000000, 131.000000)" data-v-0c35f2a4="">
+						<g id="Rectangle-22" data-v-0c35f2a4=""></g>
+						<g id="Group-66" data-v-0c35f2a4="">
+							<g
+								transform="translate(191.500000, 44.500000) rotate(-328.000000) translate(-191.500000, -44.500000) translate(102.000000, -19.000000)"
+								data-v-0c35f2a4=""
+							>
+								<path
+									id="Path-12"
+									fill="none"
+									stroke="#52A5E0"
+									stroke-width="30"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M0.92 126.49C0.92 126.49 40.12 14.49 161.92 14.49"
+									data-v-0c35f2a4=""
+								></path>
+								<circle
+									r="15"
+									cx="163.3"
+									cy="15.22"
+									id="Oval-2"
+									stroke="none"
+									fill="#000000"
+									fill-opacity="0.16"
+									fill-rule="evenodd"
+									transform="translate(163.301974, 15.224935) rotate(-360.000000) translate(-163.301974, -15.224935) "
+									data-v-0c35f2a4=""
+								></circle>
+							</g>
+						</g>
+						<g id="Group-66" data-v-0c35f2a4="">
+							<g
+								transform="translate(184.764960, 331.075976) rotate(-148.000000) translate(-184.764960, -331.075976) translate(96.264960, 267.075976)"
+								data-v-0c35f2a4=""
+							>
+								<path
+									fill="none"
+									id="Path-12"
+									stroke="#065F9E"
+									stroke-width="30"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M0.39 127.03C0.39 127.03 39.59 15.03 161.39 15.03"
+									data-v-0c35f2a4=""
+								></path>
+								<circle
+									r="15"
+									cy="15.12"
+									cx="161.83"
+									id="Oval-2"
+									stroke="none"
+									fill="#000000"
+									fill-opacity="0.16"
+									fill-rule="evenodd"
+									transform="translate(161.834814, 15.117082) rotate(-360.000000) translate(-161.834814, -15.117082) "
+									data-v-0c35f2a4=""
+								></circle>
+							</g>
+						</g>
+						<g id="Group-66" data-v-0c35f2a4="">
+							<g
+								transform="translate(44.764960, 184.075976) rotate(-58.000000) translate(-44.764960, -184.075976) translate(-44.235040, 120.575976)"
+								data-v-0c35f2a4=""
+							>
+								<path
+									fill="none"
+									id="Path-12"
+									stroke="#CEE5F6"
+									stroke-width="30"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M0.02 126.43C0.02 126.43 39.22 14.43 161.02 14.43"
+									data-v-0c35f2a4=""
+								></path>
+								<circle
+									r="15"
+									cy="15.4"
+									cx="162.01"
+									id="Oval-2"
+									stroke="none"
+									fill="#000000"
+									fill-opacity="0.16"
+									fill-rule="evenodd"
+									transform="translate(162.014468, 15.404588) rotate(-360.000000) translate(-162.014468, -15.404588) "
+									data-v-0c35f2a4=""
+								></circle>
+							</g>
+						</g>
+						<g id="Group-66" data-v-0c35f2a4="">
+							<g
+								transform="translate(331.227680, 193.919335) rotate(-237.000000) translate(-331.227680, -193.919335) translate(242.227680, 130.419335)"
+								data-v-0c35f2a4=""
+							>
+								<path
+									fill="none"
+									id="Path-12"
+									stroke="#087FD2"
+									stroke-width="30"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M0.13 126.63C0.13 126.63 39.32 14.63 161.13 14.63"
+									data-v-0c35f2a4=""
+								></path>
+								<circle
+									r="15"
+									cy="15.23"
+									cx="162.38"
+									id="Oval-2"
+									stroke="none"
+									fill="#000000"
+									fill-opacity="0.16"
+									fill-rule="evenodd"
+									transform="translate(162.376413, 15.226430) rotate(-360.000000) translate(-162.376413, -15.226430) "
+									data-v-0c35f2a4=""
+								></circle>
+							</g>
+						</g>
+					</g>
+				</g>
+			</g>
+		</g>
+	</svg>
+</template>
+
+<script></script>
+
+<style scoped></style>
