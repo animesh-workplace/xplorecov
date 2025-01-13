bd254b2303b9639a2ce336500ecc5ca947d6e784
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-13 23:21:54 +0530

    updated file - backend/analysis_engine/consumer.py

--- backend/analysis_engine/consumer.py
+++ backend/analysis_engine/consumer.py
@@ -28,8 +28,19 @@ class AnalysisConsumer(AsyncJsonWebsocketConsumer):
         is_authenticated = await self.authenticate_backend_service()
         if not is_authenticated:
             return  # Connection closed during authentication
+        
+        # Validate the expected fields in the message
+        user_id = self.scope["url_route"]["kwargs"]["user_id"]
+        analysis_id = self.scope["url_route"]["kwargs"]["analysis_id"]
+        status_update = json.loads(event["text"])
 
-        print(f"Received message: {event}")
+        if not user_id or not analysis_id or not status_update:
+            print("Missing required fields in the message")
+            return
+
+        # Update the UserAnalysis model
+        await self.update_analysis_status(user_id, analysis_id, status_update)
+
         await self.channel_layer.group_send(
             "Workshop", {"type": "task_message", "message": json.loads(event["text"])}
         )
@@ -38,11 +49,30 @@ class AnalysisConsumer(AsyncJsonWebsocketConsumer):
         data = {"message": event["message"]}
         await self.send_json(data)
 
+    @sync_to_async
+    def update_analysis_status(self, user_id, analysis_id, status_update):
+        try:
+            analysis = UserAnalysis.objects.get(user_id=user_id, analysis_id=analysis_id)
+
+
+            if not isinstance(analysis.analysis_status, list):
+                analysis.analysis_status = [status_update]
+            else:
+                # Append the new status to the existing `analysis_status`
+                analysis.analysis_status.append(status_update)
+
+            # Save the changes
+            analysis.save()
+
+        except UserAnalysis.DoesNotExist:
+            print(f"User Analysis with user_id = {user_id} and analysis_id = {analysis_id} not found")
+        except Exception as e:
+            print(f"Error updating analysis status: {e}")
+
     async def authenticate_backend_service(self):
         query_string = self.scope.get("query_string", b"").decode("utf-8")
         query_params = parse_qs(query_string)
         BACKEND_WEBSOCKET_UUID = query_params.get("BACKEND_WEBSOCKET_UUID", [None])[0]
-        print(BACKEND_WEBSOCKET_UUID)
 
         if not BACKEND_WEBSOCKET_UUID:
             # Reject the connection if the UUID is missing
