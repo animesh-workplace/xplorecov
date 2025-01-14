7eb2f697158410b345e822d55c60af6f7374d2fa
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-14 11:01:23 +0530

    updated file - backend/analysis_engine/consumer.py

--- backend/analysis_engine/consumer.py
+++ backend/analysis_engine/consumer.py
@@ -14,11 +14,13 @@ class AnalysisConsumer(AsyncJsonWebsocketConsumer):
         user_id = self.scope["url_route"]["kwargs"]["user_id"]
         analysis_id = self.scope["url_route"]["kwargs"]["analysis_id"]
 
+        existing_status = await self.get_analysis_status(user_id, analysis_id)
+
         task_id = f"{user_id}.{analysis_id}"
         try:
             await self.accept()
             await self.channel_layer.group_add(task_id, self.channel_name)
-            data = {"message": "You have connected to Analysis Real Time Updates"}
+            data = {"message": "You have connected to Analysis Real Time Updates", "current_status": existing_status}
             await self.send_json(data)
         except Exception as e:
             raise
@@ -49,6 +51,15 @@ class AnalysisConsumer(AsyncJsonWebsocketConsumer):
     async def task_message(self, event):
         data = {"message": event["message"]}
         await self.send_json(data)
+
+    @sync_to_async
+    def get_analysis_status(self, user_id, analysis_id):
+        try:
+            analysis = UserAnalysis.objects.get(user_id=user_id, analysis_id=analysis_id)
+            return analysis.analysis_status
+        except UserAnalysis.DoesNotExist:
+            return None
+
 
     @sync_to_async
     def update_analysis_status(self, user_id, analysis_id, status_update):
