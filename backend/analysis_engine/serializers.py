b28c7c90c0a632466700394b406be6bcbbfa6c01
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-14 18:16:59 +0530

    updated file - backend/analysis_engine/serializers.py

--- backend/analysis_engine/serializers.py
+++ backend/analysis_engine/serializers.py
@@ -1,11 +1,21 @@
 from .models import UserAnalysis, ToolVersion
-from rest_framework.serializers import ModelSerializer
-
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 class UserAnalysisSerializer(ModelSerializer):
     class Meta:
         model = UserAnalysis
-        fields = ["user_id", "analysis_id", "metadata", "sequence"]
+        fields = ["user_id", "analysis_id", "metadata", "sequence", "tool_version"]
+
+    def create(self, validated_data):
+        # Fetch the most recent ToolVersion
+        try:
+            validated_data['tool_version'] = ToolVersion.objects.latest('created_at')
+        except ToolVersion.DoesNotExist:
+            raise ValidationError({"tool_version": "No tool versions are available."})
+        
+        # Create and return the UserAnalysis instance
+        return super().create(validated_data)
+
 
 
 class ToolVersionSerializer(ModelSerializer):
