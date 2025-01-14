3bc58522d9f5c92994033e4fca281eae91e014ba
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-14 12:37:06 +0530

    updated file - backend/analysis_engine/urls.py

--- backend/analysis_engine/urls.py
+++ backend/analysis_engine/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SnakemakeView, UserAnalysisView
+from .views import UserAnalysisView, ToolUpdateView, ToolVersionCreateView
 
 urlpatterns = [
     path("run-main-workflow/", UserAnalysisView.as_view(), name="run-main-workflow"),
+    path("update-tool-version/", ToolVersionCreateView.as_view(), name="update-tool-version"),
+    path("run-tool-update-workflow/", ToolUpdateView.as_view(), name="run-tool-update-workflow"),
 ]
