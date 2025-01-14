a56dffa77456830a62849cea1a1cd7b42ef5fc11
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-14 12:37:06 +0530

    updated file - backend/analysis_engine/serializers.py

--- backend/analysis_engine/serializers.py
+++ backend/analysis_engine/serializers.py
@@ -1,4 +1,4 @@
-from .models import UserAnalysis
+from .models import UserAnalysis, ToolVersion
 from rest_framework.serializers import ModelSerializer
 
 
@@ -6,3 +6,10 @@ class UserAnalysisSerializer(ModelSerializer):
     class Meta:
         model = UserAnalysis
         fields = ["user_id", "analysis_id", "metadata", "sequence"]
+
+
+class ToolVersionSerializer(ModelSerializer):
+    class Meta:
+        model = ToolVersion
+        fields = ['nextclade_version', 'pangolin_version', 'constellations_version', 'scorpio_version', 
+                  'usher_version', 'gofasta_version', 'minimap2_version', 'faToVcf_version', 'created_at']
