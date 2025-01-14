f5fca52027bb07674a786334cd31fe9d10d4ce59
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-14 22:54:00 +0530

    updated file - backend/analysis_engine/serializers.py

--- backend/analysis_engine/serializers.py
+++ backend/analysis_engine/serializers.py
@@ -1,10 +1,18 @@
 from .models import UserAnalysis, ToolVersion
 from rest_framework.serializers import ModelSerializer, ValidationError
 
+class ToolVersionSerializer(ModelSerializer):
+    class Meta:
+        model = ToolVersion
+        fields = ['nextclade_version', 'pangolin_version', 'constellations_version', 'scorpio_version', 
+                  'usher_version', 'gofasta_version', 'minimap2_version', 'faToVcf_version', 'created_at']
+
 class UserAnalysisSerializer(ModelSerializer):
+    # tool_version = ToolVersionSerializer()
+
     class Meta:
         model = UserAnalysis
-        fields = ["user_id", "analysis_id", "metadata", "sequence", "tool_version"]
+        fields = ["user_id", "analysis_id", "metadata", "sequence", "submission_date"]
 
     def create(self, validated_data):
         # Fetch the most recent ToolVersion
@@ -15,11 +23,3 @@ class UserAnalysisSerializer(ModelSerializer):
         
         # Create and return the UserAnalysis instance
         return super().create(validated_data)
-
-
-
-class ToolVersionSerializer(ModelSerializer):
-    class Meta:
-        model = ToolVersion
-        fields = ['nextclade_version', 'pangolin_version', 'constellations_version', 'scorpio_version', 
-                  'usher_version', 'gofasta_version', 'minimap2_version', 'faToVcf_version', 'created_at']
