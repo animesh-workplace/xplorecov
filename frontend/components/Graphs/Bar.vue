b11738470054ad5379c3d23dfe8a1c949bcaa8dd
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-18 18:06:47 +0530

    added file - frontend/components/Graphs/Bar.vue

---
+++ frontend/components/Graphs/Bar.vue
@@ -0,0 +1,75 @@
+<template>
+	<div>
+		<Skeleton class="mb-2" height="24rem" v-if="isLoading" />
+		<VChart ref="chart" class="w-[22rem] h-96" :option="graph_options" :class="{ hidden: isLoading }" />
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+	rawData: { type: Object, required: true, default: {} },
+})
+
+const chart = templateRef('chart')
+const values = Object.values(props.rawData?.data)
+const categories = Object.keys(props.rawData?.data)
+
+const graph_options = ref({
+	yAxis: {
+		type: 'value',
+		axisLabel: {
+			fontFamily: 'Averta',
+			fontWeight: 500,
+		},
+	},
+	tooltip: {
+		trigger: 'axis',
+		position: 'right',
+		renderMode: 'richText',
+		axisPointer: { type: 'shadow' },
+		borderColor: '#fff',
+		textStyle: {
+			fontFamily: 'Averta',
+			fontWeight: 500,
+		},
+		order: 'valueDesc',
+	},
+	animation: true,
+	title: {
+		text: props.rawData?.name,
+		textStyle: {
+			width: 400,
+			lineHeight: 20,
+			overflow: 'break',
+		},
+	},
+	grid: {
+		left: '0%',
+		right: '0%',
+		bottom: '0%',
+		containLabel: true,
+	},
+	series: [{ type: 'bar', data: values }],
+	itemStyle: { borderRadius: [5, 5, 0, 0] },
+	xAxis: {
+		type: 'category',
+		data: categories,
+		axisLabel: {
+			fontFamily: 'Averta',
+			fontWeight: 500,
+		},
+	},
+})
+
+const isLoading = ref(true)
+
+onMounted(() => {
+	nextTick(() => {
+		setTimeout(() => {
+			isLoading.value = false
+		}, 2000)
+	})
+})
+</script>
+
+<style scoped></style>
