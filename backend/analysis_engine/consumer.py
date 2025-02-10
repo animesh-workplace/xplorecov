f5296ab09b232bca71dcfba549196e9c278727de
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-02-10 13:29:19 +0530

    updated file - backend/analysis_engine/consumer.py

--- backend/analysis_engine/consumer.py
+++ backend/analysis_engine/consumer.py
@@ -129,24 +129,24 @@ class AnalysisConsumer(AsyncJsonWebsocketConsumer):
         return WebSocketBackendUUID.objects.filter(uuid=uuid).exists()
 
 
-class ChatConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
-        # Get user_id and analysis_id from the URL parameters
-        user_id = self.scope["url_route"]["kwargs"]["user_id"]
-        analysis_id = self.scope["url_route"]["kwargs"]["analysis_id"]
+# class ChatConsumer(AsyncJsonWebsocketConsumer):
+#     async def connect(self):
+#         # Get user_id and analysis_id from the URL parameters
+#         user_id = self.scope["url_route"]["kwargs"]["user_id"]
+#         analysis_id = self.scope["url_route"]["kwargs"]["analysis_id"]
 
-        task_id = f"{user_id}.{analysis_id}.chat.llm"
-        try:
-            await self.accept()
-            await self.channel_layer.group_add(task_id, self.channel_name)
-            data = {"message": "You have connected to LLM Real Time Updates"}
-            await self.send_json(data)
-        except Exception as e:
-            raise
+#         task_id = f"{user_id}.{analysis_id}.chat.llm"
+#         try:
+#             await self.accept()
+#             await self.channel_layer.group_add(task_id, self.channel_name)
+#             data = {"message": "You have connected to LLM Real Time Updates"}
+#             await self.send_json(data)
+#         except Exception as e:
+#             raise
 
-    async def websocket_receive(self, event):
-        return
+#     async def websocket_receive(self, event):
+#         return
 
-    async def chat_message(self, event):
-        data = {"message": event["message"]}
-        await self.send_json(data)
+#     async def chat_message(self, event):
+#         data = {"message": event["message"]}
+#         await self.send_json(data)
