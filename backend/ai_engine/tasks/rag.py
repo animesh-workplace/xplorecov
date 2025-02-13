872a602ef2630e80a983ab44508c5b964dcd7511
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-02-13 14:59:49 +0530

    updated file - backend/ai_engine/tasks/rag.py

--- backend/ai_engine/tasks/rag.py
+++ backend/ai_engine/tasks/rag.py
@@ -40,54 +40,29 @@ def ask_ai_for_code(content, user_analysis_id, user_id, analysis_id):
 
     chain = AgentChain()
     result = chain.process_request(content, db_schema)
-    print("Resultt", result)
 
-    # if result.success:
-    #     print(result.data["summary"])
-    # else:
-    #     print(f"Error: {result.message}")
+    if result.success:
+        result_content = result.data["summary"]
+    else:
+        result_content = result.message
 
-    # client = OpenAI(
-    #     base_url="http://10.10.6.80/xplorecov/ai/code/v1",
-    #     api_key="sk-no-key-required",
-    # )
-    # completion = client.chat.completions.create(
-    #     temperature=1,
-    #     model="LLaMA_CPP",
-    #     messages=[
-    #         {
-    #             "role": "system",
-    #             "content": """
-    #                 You are an AI assistant specifically validates if the request is safe and feasible to process. Returns TRUE/FALSE ONLY nothing else.
-    #             """,
-    #         },
-    #         {"role": "user", "content": content},
-    #     ],
-    # )
-
-    # print(completion.choices[0].message.content)
-
-    # santized_message_content = re.sub(
-    #     r"\n+", " ", completion.choices[0].message.content
-    # ).strip()
-
-    # chat_message_object = {
-    #     "sender": "assistant",
-    #     "content_type": "text",
-    #     "parent_message_uuid": None,
-    #     "user_analysis": user_analysis_id,
-    #     "content": json.dumps(santized_message_content),
-    # }
-    # serializer = ChatMessagesSerializer(data=chat_message_object)
-    # if serializer.is_valid():
-    #     serializer.save()
-    #     channel_layer = get_channel_layer()
-    #     async_to_sync(channel_layer.group_send)(
-    #         f"{user_id}.{analysis_id}.chat.llm",
-    #         {
-    #             "type": "chat_message",
-    #             "message": santized_message_content,
-    #         },
-    #     )
-    # else:
-    #     print(serializer.errors)
+    chat_message_object = {
+        "sender": "assistant",
+        "content_type": "text",
+        "parent_message_uuid": None,
+        "user_analysis": user_analysis_id,
+        "content": json.dumps(result_content),
+    }
+    serializer = ChatMessagesSerializer(data=chat_message_object)
+    if serializer.is_valid():
+        serializer.save()
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            f"{user_id}.{analysis_id}.chat.llm",
+            {
+                "type": "chat_message",
+                "message": json.dumps(result_content),
+            },
+        )
+    else:
+        print(serializer.errors)
