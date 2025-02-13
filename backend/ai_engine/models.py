f798138341bca62697de3999b05d9b15f5679f43
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-02-13 16:10:24 +0530

    updated file - backend/ai_engine/models.py

--- backend/ai_engine/models.py
+++ backend/ai_engine/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class LLMCache(models.Model):
+    modified_query = models.TextField()
+    generated_code = models.TextField()
+    hit_count = models.IntegerField(default=1)
+    user_query = models.TextField(unique=True)
+    query_embedding = models.BinaryField(null=True)
+    last_accessed = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["user_query"]),
+        ]
