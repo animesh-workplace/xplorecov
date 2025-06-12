b469aa9e7726cb2f21c15d4f516a29a059fa0921
Animesh <animesh.workplace@gmail.com>
2025-06-12 21:53:07 +0530

    updated file - frontend/tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
-const fontFamily = defaultTheme.fontFamily
-fontFamily['sans'] = ['Averta', 'system-ui']
 
 module.exports = {
-	theme: {},
-	darkMode: ['class'],
 	content: [
 		'./pages/**/*.vue',
 		'./layouts/**/*.vue',
 		'./nuxt.config.{js,ts}',
 		'./plugins/**/*.{js,ts}',
 		'./components/**/*.{js,vue,ts}',
-		'./node_modules/flowbite.{js,ts}',
+		'./node_modules/flowbite/**/*.js',
 	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Averta', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
 	plugins: [require('tailwind-scrollbar')],
 }
