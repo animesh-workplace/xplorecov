d411175322c0a712a0264672fa959d9cea034220
Animesh <animesh.workplace@gmail.com>
2025-06-14 19:55:45 +0530

    updated file - frontend/components/NavBar.vue

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -44,29 +44,32 @@
 				id="navbar-sticky"
 			>
 				<ul
-					class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-neutral-800 dark:border-gray-700"
+					class="items-center flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-neutral-800 dark:border-gray-700"
 				>
 					<li>
 						<NuxtLink
 							to="/"
-							class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+							class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:dark:hover:text-blue-500 md:hover:text-blue-700 md:p-0"
 							aria-current="page"
-							>Home</NuxtLink
 						>
+							Home
+						</NuxtLink>
 					</li>
 					<li>
 						<NuxtLink
 							to="/upload"
 							class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>Upload</NuxtLink
 						>
+							Upload
+						</NuxtLink>
 					</li>
 					<li>
 						<NuxtLink
 							to="/analysis"
 							class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>Analysis</NuxtLink
 						>
+							Analysis
+						</NuxtLink>
 					</li>
 					<li>
 						<Button
